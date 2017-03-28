03912d9992108782deda6922b8c47f4abd554aa1
jwgsbbc <james.shephard@bbc.co.uk>
2017-03-28 09:19:05 +0100

    Added Poco lib .a's (armeabi only) Started trying to make a network call TODO: remove hard-coded fully qualified paths to POCO includes (not in project). Fixed dependancy issue by making sure Poco libs were in the right order in the CMakeLists.txt

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,16 @@
 #include <string>
 #import <vector>
 #include "TestNewClass.h"
+#include "Poco/Timestamp.h"
+#include "Poco/Net/HTTPClientSession.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Net/HTTPResponse.h"
+
+using Poco::Timestamp;
+using Poco::Net::HTTPClientSession;
+using Poco::Net::HTTPRequest;
+using Poco::Net::HTTPResponse;
+using Poco::Net::HTTPMessage;
 
 extern "C" JNIEXPORT jstring JNICALL Java_uk_co_bbc_jplayer_jplayerapp_NativeBridge_staticStringFromJNI(JNIEnv *env, jclass type) {
     std::string hello = "Hello from C++ 1";
@@ -20,10 +30,31 @@ jbyteArray createJavaByteArrayCopy(JNIEnv *env, std::vector<char> &imageBytes) {
 }
 
 std::vector<char> readImageBytes(const char *id) {
+
+    HTTPClientSession session;
+    HTTPRequest request(HTTPRequest::HTTP_GET, "http://iplayer-mobile-appconfig.files.bbci.co.uk/config/iplayer/android/4.22.2/config.json", HTTPMessage::HTTP_1_1);
+
+    HTTPResponse response;
+
+    try {
+        session.sendRequest(request);
+    }
+    catch (const std::exception& e) {
+        const char *string = e.what();
+        int i=0;
+    }
+
+    std::istream& rs = session.receiveResponse(response);
+    HTTPResponse::HTTPStatus status = response.getStatus();
+
     TestNewClass testNewClass;
 
-
+    Timestamp timestamp;
     
+    timestamp.update();
+
+    time_t i = timestamp.epochTime();
+
     char a = testNewClass.getOne();
 
     auto vector1 = std::vector<char>();
